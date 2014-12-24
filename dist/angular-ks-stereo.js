3c079c82134fcbd5a1470147f1b247db6e172945
kmsheng <kmsh3ng@gmail.com>
2014-12-25 04:13:30 +0800

    update dist folder

--- dist/angular-ks-stereo.js
+++ dist/angular-ks-stereo.js
@@ -937,10 +937,6 @@
       // jshint maxstatements: 50
       link: function(scope, element) {
 
-        var IMAGE_PATH = ksAudio.defaults.imagePath;
-        var OPENHAND_CURSOR_PATH = IMAGE_PATH + '/openhand.cur';
-        var CLOSEHAND_CURSOR_PATH = IMAGE_PATH + '/closehand.cur';
-
         var parent = element.parent();
         var canvasElem = element.find('canvas');
         var canvas = canvasElem[0];
@@ -1058,15 +1054,15 @@
 
         var cursor = {
           mousedown: false,
-          getValue: function() {
+          getCssClass: function() {
             var center = belt.center;
             if (cursor.inCircle(center.x, center.y, belt.radius)) {
               if (cursor.mousedown) {
-                return 'url(' + CLOSEHAND_CURSOR_PATH + '), pointer';
+                return 'cursor-closehand';
               }
-              return 'url(' + OPENHAND_CURSOR_PATH + '), pointer';
+              return 'cursor-openhand';
             }
-            return 'default';
+            return 'cursor-default';
           },
           x: null,
           y: null,
@@ -1081,7 +1077,7 @@
         };
 
         function setCursorImage() {
-          canvasElem.css('cursor', cursor.getValue());
+          canvas.className = cursor.getCssClass();
         }
 
         function getCentralAngle(canvas, cursor) {
@@ -1123,7 +1119,7 @@
           }
 
           cursor.mousedown = true;
-          canvasElem.css('cursor', 'url(' + CLOSEHAND_CURSOR_PATH + '), pointer');
+          setCursorImage();
 
           (function animate() {
             if (cursor.mousedown) {
