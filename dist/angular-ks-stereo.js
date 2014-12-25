483b79d7c7a27b2803fd670aff3367cba67385ae
kmsheng <kmsh3ng@gmail.com>
2014-12-25 15:08:51 +0800

    update dist/

--- dist/angular-ks-stereo.js
+++ dist/angular-ks-stereo.js
@@ -1076,7 +1076,12 @@
         };
 
         function setCursorImage() {
-          canvas.className = cursor.getCssClass();
+
+          var className = cursor.getCssClass();
+
+          if (className !== canvas.className) {
+            canvas.className = className;
+          }
         }
 
         function getCentralAngle(canvas, cursor) {
